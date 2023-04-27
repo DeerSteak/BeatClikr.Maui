a64f003fd3b274559c7b8bd2e5e4292286418be9
Ben Funk <bfunk1978@gmail.com>
2023-04-27 09:31:41 -0500

    use constants for translation values

--- BeatClikr.Maui/CustomControls/MetronomeClickerView.xaml.cs
+++ BeatClikr.Maui/CustomControls/MetronomeClickerView.xaml.cs
@@ -3,7 +3,9 @@
 public partial class MetronomeClickerView : ContentView
 {
     private int translateTo = 235;
+    private int translateFrom = 0;
     private uint beatLengthInMs = 500;
+    private const int TRANSLATION_MAX = 235;
     public MetronomeClickerView(ViewModels.MetronomeClickerViewModel viewModel)
     {
         InitializeComponent();
@@ -19,8 +21,10 @@ public partial class MetronomeClickerView : ContentView
 
     private void Animate()
     {
+        BeatBox.TranslationX = translateFrom;
         BeatBox.TranslateTo(translateTo, 0, beatLengthInMs, Easing.Linear);
-        translateTo = translateTo == 235 ? 0 : 235;
+        translateTo = translateTo == TRANSLATION_MAX ? 0 : TRANSLATION_MAX;
+        translateFrom = translateTo == TRANSLATION_MAX ? 0 : TRANSLATION_MAX;
     }
 
     private void SetBeatMilliseconds(uint milliseconds)
