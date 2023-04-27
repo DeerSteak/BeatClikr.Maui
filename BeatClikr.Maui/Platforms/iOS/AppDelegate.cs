e5f94ef8484bb43dbbf59c39ee2d9bb86e0c5165
Ben Funk <ben.funk@itv-3.com>
2023-04-27 13:42:33 -0500

    Forgot to set grid row

--- BeatClikr.Maui/Platforms/iOS/AppDelegate.cs
+++ BeatClikr.Maui/Platforms/iOS/AppDelegate.cs
@@ -12,19 +12,10 @@ public class AppDelegate : MauiUIApplicationDelegate
 
     public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
     {
-        var testId = "F8BB1C28-BAE8-11D6-9C31-00039315CD46";
-        
-        MobileAds.SharedInstance.RequestConfiguration.TestDeviceIdentifiers = new string[] { testId };
-
-        MobileAds.SharedInstance.Start(AdsCompletionHandler);
-
+        MobileAds.SharedInstance.Start(CompletionHandler);
         return base.FinishedLaunching(application, launchOptions);
     }
 
-    private void AdsCompletionHandler(InitializationStatus status)
-    {
-        var trackerService = ServiceHelper.GetService<Services.Interfaces.IAdTrackingHandlerService>();
-        Task.Run(async () => await trackerService?.AskTrackingPermission());
-    }
+    private void CompletionHandler(InitializationStatus status) { }
 }
 
