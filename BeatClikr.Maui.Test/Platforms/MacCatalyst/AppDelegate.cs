b85ee8602f203a6d92cc332111ad0be588a9ba4d
Ben Funk <Benjamin.Funk@atmosera.com>
2023-05-31 08:59:22 -0500

    functional unit testing setup

---
+++ BeatClikr.Maui.Test/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BeatClikr.Maui.Test
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
